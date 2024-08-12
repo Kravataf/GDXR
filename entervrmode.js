e0c05a925599255f444f1d2bbaa115a8c5b403f4
Kravataf <jurajsoltyss@gmail.com>
2024-08-12 21:06:46 +0200

    Update entervrmode.js

--- entervrmode.js
+++ entervrmode.js
@@ -2,18 +2,12 @@ const script = document.createElement('script');
 script.src = 'https://cdn.jsdelivr.net/npm/webxr-polyfill@1.2.0/build/webxr-polyfill.min.js';
 document.head.appendChild(script);
 
-//webXR lib
+async function enterVRMode() {
+    const session = await navigator.xr.requestSession('immersive-vr');
+    const gl = canvas.getContext('webgl', { xrCompatible: true });
+    const referenceSpace = await session.requestReferenceSpace('local-floor');
+    const renderer = new WebXRRenderer(gl, session, referenceSpace);
+    renderer.render();
+}
 
-//get xr sesh
-const session = await navigator.xr.requestSession('immersive-vr');
-
-//create webgl content
-const gl = canvas.getContext('webgl', { xrCompatible: true });
-
-const referenceSpace = await session.requestReferenceSpace('local-floor');
-
-//renderer
-const renderer = new WebXRRenderer(gl, session, referenceSpace);
-
-//render scene
-renderer.render();
+enterVRMode(); //call func to start vr mode
