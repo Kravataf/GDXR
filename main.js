04a50b1bc1d344c945a494940edbd3d409b02e8d
Kravataf <jurajsoltyss@gmail.com>
2024-12-12 14:48:53 +0100

    i guess checking it like this works?

--- main.js
+++ main.js
@@ -10,13 +10,3 @@ async function startXRSession() {
         console.log('not supported');
     }
 }
-//attach the startXRSession function to a user interaction event!!
-const startButton = document.createElement('button');
-startButton.textContent = 'Start XR Session';
-startButton.style.position = 'absolute';
-startButton.style.top = '10px';
-startButton.style.left = '10px';
-startButton.addEventListener('click', () => {
-    startXRSession();
-});
-document.body.appendChild(startButton);
