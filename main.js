399b5486371fac2f0b94449ea4a8b50041741f54
Kravataf <jurajsoltyss@gmail.com>
2024-12-13 12:30:02 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 // it since gdev already has threejs so im not sure if its a good idea to import it again..
 // import * as THREE from 'https://cdn.jsdelivr.net/npm/three.js@0.77.1/shim.min.js'; //i  hope this works
 
-altImport('https://cdn.jsdelivr.net/npm/three@0.77.1/build/three.min.js', function() {});
+loadScript('https://cdn.jsdelivr.net/npm/three@0.77.1/build/three.min.js', function() {});
 
 import { VRButton } from 'three/addons/webxr/VRButton.js';
 
