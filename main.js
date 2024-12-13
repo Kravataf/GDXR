8c07909643ae9f87651176c5303d54fb0476f6f2
Kravataf <jurajsoltyss@gmail.com>
2024-12-13 13:02:16 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,5 @@
-//import some stuff importing it like this wont work but idk how to do 
-// it since gdev already has threejs so im not sure if its a good idea to import it again..
-// import * as THREE from 'https://cdn.jsdelivr.net/npm/three.js@0.77.1/shim.min.js'; //i  hope this works
 // loadTHREEJS('https://cdn.jsdelivr.net/npm/three@0.77.1/build/three.min.js', function() {});
-
-// const loadTHREEJS = document.createElement("script");
-// loadTHREEJS.src = "https://cdn.jsdelivr.net/npm/three@0.77.1/build/three.min.js";
-// document.head.appendChild(loadTHREEJS);
-
 // import { VRButton } from 'three/addons/webxr/VRButton.js';
-gdjs.__WithThreeJS.set("BuiltIn3D", (runtimeScene.getRenderer()._threeRenderer ? false : true)); //this should enable threejs if false
 async function startXRSession() {
     if (navigator.xr) {
         try {
@@ -28,17 +19,17 @@ async function startXRSession() {
 
 // } );
 //attach the startXRSession function to a user interaction event!!
-// const startButton = document.createElement('button');
-// startButton.textContent = 'Start XR Session';
-// startButton.style.position = 'absolute';
-// startButton.style.top = '10px';
-// startButton.style.left = '10px';
-// startButton.addEventListener('click', () => {
-//     startXRSession();
-// });
-// document.body.appendChild(startButton);
+const startButton = document.createElement('button');
+startButton.textContent = 'Start XR Session';
+startButton.style.position = 'absolute';
+startButton.style.top = '10px';
+startButton.style.left = '10px';
+startButton.addEventListener('click', () => {
+    startXRSession();
+});
+document.body.appendChild(startButton);
 
-document.body.appendChild( VRButton.createButton( renderer ) ); //button from the docs https://threejs.org/docs/#manual/en/introduction/How-to-create-VR-content
+// document.body.appendChild( VRButton.createButton( renderer ) ); //button from the docs https://threejs.org/docs/#manual/en/introduction/How-to-create-VR-content
 
 //webxr doesnt work bc of insecure connection or whatever
 if (!location.protocol.includes('https')) {
