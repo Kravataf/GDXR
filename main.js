e60c0e32ac7a123cb28050545a4ef58c35e12d2d
Kravataf <jurajsoltyss@gmail.com>
2024-12-13 12:49:14 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,12 @@
 // it since gdev already has threejs so im not sure if its a good idea to import it again..
 // import * as THREE from 'https://cdn.jsdelivr.net/npm/three.js@0.77.1/shim.min.js'; //i  hope this works
 
-// loadScript('https://cdn.jsdelivr.net/npm/three@0.77.1/build/three.min.js', function() {});
+loadExternalScript('https://cdn.jsdelivr.net/npm/three@0.77.1/build/three.min.js', function() {
+    console.log('three.js loaded!');
+    // Initialize WebXR or other features here after loading the script
+    startXR();
+});
+
 
 import { VRButton } from 'three/addons/webxr/VRButton.js';
 async function startXRSession() {
