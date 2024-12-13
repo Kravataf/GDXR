1afe43547ef85c62970bd69569b6a241a104b0c2
Kravataf <jurajsoltyss@gmail.com>
2024-12-13 11:57:19 +0100

    why

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-//import some stuff       vvvvvv wont work probably
+//import some stuff importing it like this wont work but idk how to do 
+// it since gdev already has threejs so im not sure if its a good idea to import it again..
 import { VRButton } from 'three/addons/webxr/VRButton.js';
 
 async function startXRSession() {
@@ -6,6 +7,7 @@ async function startXRSession() {
         try {
             const session = await navigator.xr.requestSession('immersive-vr');
             console.log('webxr session started:', session);
+            renderer.xr.enabled = true;
         } catch (err) {
             console.error('failed to start:', err);
         }
@@ -14,14 +16,14 @@ async function startXRSession() {
     }
 }
 //attach the startXRSession function to a user interaction event!!
-const startButton = document.createElement('button');
-startButton.textContent = 'Start XR Session';
-startButton.style.position = 'absolute';
-startButton.style.top = '10px';
-startButton.style.left = '10px';
-startButton.addEventListener('click', () => {
-    startXRSession();
-});
+// const startButton = document.createElement('button');
+// startButton.textContent = 'Start XR Session';
+// startButton.style.position = 'absolute';
+// startButton.style.top = '10px';
+// startButton.style.left = '10px';
+// startButton.addEventListener('click', () => {
+//     startXRSession();
+// });
 document.body.appendChild(startButton);
 //webxr doesnt work bc of insecure connection or whatever
 if (!location.protocol.includes('https')) {
@@ -29,4 +31,3 @@ if (!location.protocol.includes('https')) {
       "ur connection is not secure and WebXR wont work!! please enable the 'Insecure origins treated as secure' flag in Chrome (chrome://flags)."
     );
   }
-//ako mam zmenit nech gd pouziva xr cameru??
